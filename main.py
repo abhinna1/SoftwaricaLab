c875c33ae9e3f7038db1153215871a31b99d7157
manan <manandharabhinna@gmail.com>
2021-02-23 09:37:08 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Write a program that takes three numbers and prints their sum and prints their sums.
+Every number is given on a seperate line.
+'''
+
+
+num1=int(input('enter first number'))
+num2=int(input('enter second number'))
+num3=int(input('enter third'))
+sum=num1+num2+num3
+print('The sum of all three numbers is ',sum)
+
+''' Write a program that reads the length of the base and the height of a right-angled triangle and points the area.
+Every number is given on a seperate line. '''
+
+base = int(input('enter base of the right angled triangle'))
+height = int(input('enter height of the right angled triangle'))
+area = int(0.5 * base * height)
+print('The area of the triange is ', area)
+
+'''N students take K apples and distribute them among each other evenly.
+The remaining (the undivisible) part remains in the basket. How mant apples will each single student get? 
+ How many apples will remain in the basket? The program reads the number N and K. 
+ It should print the two answers for the questions above'''
