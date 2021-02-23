34529104dbe551dc8cfdba67399c6a7547e5be0f
manan <manandharabhinna@gmail.com>
2021-02-23 10:05:55 +0545

    question 3

---
+++ Python Sum/sum.py
@@ -0,0 +1,15 @@
+'''
+Write a program that takes three numbers and prints their sum and prints their sums.
+Every number is given on a seperate line.
+'''
+
+
+num1=int(input('enter first number'))
+num2=int(input('enter second number'))
+num3=int(input('enter third'))
+sum=num1+num2+num3
+print('The sum of all three numbers is ',sum)
+
+
+
+
